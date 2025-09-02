11ed6167afd9701a71518e18f55bfad247b73916
Maemi <60872762+NewMassMedia@users.noreply.github.com>
2025-09-02 23:08:47 +0900

    Add scriptable renderer feature template

--- ScriptGenerator.cs
+++ ScriptGenerator.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
 using UnityEditor;
 using System.IO;
-using System.Linq;
+using System.Runtime.CompilerServices;
+using UnityEditor.Experimental;
+using UnityEditor.ProjectWindowCallback;
+using UnityEngine;
 
 public static class ScriptGenerator
 {
@@ -12,6 +16,7 @@ public static class ScriptGenerator
     private const string INTERFACE = "Interface";
     private const string SCRIPTABLE_OBJECT = "ScriptableObject";
     private const string STRUCT = "Struct";
+    private const string RENDERER_FEATURE = "RendererFeature";
 
     private static class MenuPriority
     {
@@ -22,19 +27,11 @@ public static class ScriptGenerator
         public const int INTERFACE = SCRIPT_MENU_PRIORITY + 3;
         public const int SCRIPTABLE_OBJECT = SCRIPT_MENU_PRIORITY + 4;
         public const int STRUCT = SCRIPT_MENU_PRIORITY + 5;
+        public const int RENDERER_FEATURE = SCRIPT_MENU_PRIORITY + 6;
     }
-    
-    private static string GetScriptPath(string className)
-    {
-        var guids = AssetDatabase.FindAssets(className + " t:script");
-        return guids.Length == 0 ? null : AssetDatabase.GUIDToAssetPath(guids.First());
-    }
-    
-    private static string GetScriptDirectory(string name)
-    {
-        var path = GetScriptPath(name);
-        return Path.GetDirectoryName(path);
-    }
+
+    private static string ScriptFolderPath => RootPath();
+    private static string RootPath([CallerFilePath] string path = "") => Path.GetDirectoryName(path);
     
     private static string ToTextFileName(string name) => $"{name}.txt";
     private static string ToNewFileName(string name) => $"New{name}.cs";
@@ -52,8 +49,7 @@ public static class ScriptGenerator
             }
         }
 
-        var templateDir = GetScriptDirectory(nameof(ScriptGenerator));
-        var templatePath = Path.Combine(templateDir, templateName);
+        var templatePath = Path.Combine(ScriptFolderPath, templateName);
 
         ProjectWindowUtil.CreateScriptAssetFromTemplateFile(
             templatePath, Path.Combine(selectedPath, ToNewFileName(defaultFileName))
@@ -81,5 +77,55 @@ public static class ScriptGenerator
     private static void CreateStructScript()
         => CreateScriptFromTemplate(ToTextFileName(STRUCT), STRUCT);
 
+    [MenuItem(itemName: SCRIPT_MENU_PATH + RENDERER_FEATURE, priority = MenuPriority.RENDERER_FEATURE)]
+    private static void CreateRendererFeatureScript()
+    {
+        ProjectWindowUtil.StartNameEditingIfProjectWindowExists(
+            0, 
+            ScriptableObject.CreateInstance<RendererFeatureCreateSequence>(), 
+            RENDERER_FEATURE, EditorGUIUtility.IconContent(EditorResources.emptyFolderIconName).image as Texture2D, 
+            (string) null);
+    }
+    
+    private class RendererFeatureCreateSequence : EndNameEditAction
+    {
+        public override void Action(int instanceId, string pathName, string resourceFile)
+        {
+            var directory = Path.GetDirectoryName(pathName);
+            var folderName = Path.GetFileName(pathName);
+            var createdFolderId = AssetDatabase.CreateFolder(directory, folderName);
+
+            var featureName = folderName + RENDERER_FEATURE;
+            var scripts = new Dictionary<string, string>
+            {
+                [featureName] = RENDERER_FEATURE,
+                [featureName + ".Pass"] = "RendererPass",
+                [featureName + ".Setting"] = "RendererSetting"
+            };
+
+            foreach (var (scriptName, templateName) in scripts)
+            {
+                CreateScriptFromTemplate(pathName, featureName, scriptName, templateName);
+            }
+
+            var folderAssetPath = AssetDatabase.GUIDToAssetPath(createdFolderId);
+            ProjectWindowUtil.ShowCreatedAsset(AssetDatabase.LoadAssetAtPath<Object>(folderAssetPath));
+        }
+
+        private void CreateScriptFromTemplate(string folderPath, string featureName, string scriptName, string templateName)
+        {
+            const string replaceToken = "#SCRIPTNAME#";
+
+            var templateFolder = Path.Combine(ScriptFolderPath, RENDERER_FEATURE);
+            var templatePath = Path.Combine(templateFolder, ToTextFileName(templateName));
+
+            var scriptContent = File.ReadAllText(templatePath).Replace(replaceToken, featureName);
+
+            var scriptPath = Path.Combine(folderPath, scriptName + ".cs");
+
+            ProjectWindowUtil.CreateScriptAssetWithContent(scriptPath, scriptContent);
+        }
+    }
+    
     #endregion Create Script
 }
